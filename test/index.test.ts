03d6041d66c8c29ee1d890cdcb5a7b82263cb102
Alaister Young <a@alaisteryoung.com>
2022-06-06 16:30:39 +1000

    feat(bar): adds const bar

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,10 @@
-import { test, assert } from "vitest"
-import { foo } from "../src"
+import { assert, test } from "vitest"
+import { bar, foo } from "../src"
 
 test("simple", () => {
   assert.equal(foo, "foo")
+})
+
+test("simple 2", () => {
+  assert.equal(bar, "bar")
 })
